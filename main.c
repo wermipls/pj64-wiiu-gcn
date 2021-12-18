28eef4d6eb11f87524047699164d0243b23fbf92
wermi <32251376+wermipls@users.noreply.github.com>
2021-12-18 12:22:59 +0100

    increment version

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #define WIN32_LEAN_AND_MEAN
 
 #define PLUGIN_NAME "pj64-wiiu-gcn"
-#define PLUGIN_VERSION "0.2.0"
+#define PLUGIN_VERSION "0.2.1"
 #define PLUGIN_NAMEVER PLUGIN_NAME " v" PLUGIN_VERSION
 #define PLUGIN_REPO "https://github.com/wermipls/pj64-wiiu-gcn"
 
